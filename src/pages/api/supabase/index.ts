74606216a40712f4d8beb8ae98e7018abf313061
Wei Lai <wlai@liveperson.com>
2023-10-03 15:54:37 -0400

    extract to a method

--- src/pages/api/supabase/index.ts
+++ src/pages/api/supabase/index.ts
@@ -1,13 +1,16 @@
-import { supabase } from "../../supabase";
+import { supabase, getGroups } from "../../supabase";
 
 // const {data} = await supabase.rpc("draw_name", {groupid: group.id, username: userName}).single()
 
 export async function GET({}) {
-  const { data, error: groupError } = await supabase.from("groups").select("*");
+//   const { data, error: groupError } = await supabase.from("groups").select("*");
+const data = await getGroups()
+console.log("my data")
+console.log(JSON.stringify(data))
 
-  if (groupError) {
-    console.error(groupError);
-  }
+//   if (groupError) {
+//     console.error(groupError);
+//   }
 
   if (!data) {
     console.error("No data returned");
--- src/pages/supabase.ts
+++ src/pages/supabase.ts
@@ -3,3 +3,9 @@ import type { Database } from './types'
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
+
+export const getGroups = async () => {
+  const { data, error: groupError } = await supabase.from("groups").select("*");
+  console.log(JSON.stringify(data));
+  return data;
+}
